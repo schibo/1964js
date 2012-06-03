3cd9824277e2d1548fcf0f8ae077aa228a7f23dd
schibo4@gmail.com <schibo4@gmail.com@0378edba-076e-5dc0-2bb2-d87a714dcd81>
2012-06-03 03:25:48 +0000

    compile.sh changes to support refactored code
    git-svn-id: http://1964js.googlecode.com/svn/trunk@32 0378edba-076e-5dc0-2bb2-d87a714dcd81

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ mkdir bin/js/
 mkdir bin/js/lib/
 mkdir bin/js/lib/bitjs/
 
-java -jar compiler.jar --js js/lib/BigInt.js --js js/lib/closure/goog/base.js  --js js/lib/Stats.js --js js/helpers.js --js js/1964.js --js js/opcodeMap.js --js js/boot.js --js js/lib/closure/goog/math/long.js --js js/constants.js --js js/pif.js --js js/memory.js --js js/dma.js --js js/interrupts.js --js js/lib/glMatrix-0.9.5.min.js --js js/lib/webgl-utils.js --js js/gfxHelpers.js --js js/videoHLE.js --js js/webGL.js --js js/tests.js --js js/lib/bitjs/io.js --js js/lib/bitjs/archive.js --js js/ui.js --js_output_file bin/js/1964js-0.0.3.min.js
+java -jar compiler.jar --js js/lib/BigInt.js --js js/lib/closure/goog/base.js  --js js/lib/Stats.js --js js/1964.js --js js/helpers.js --js js/opcodeMap.js --js js/boot.js --js js/lib/closure/goog/math/long.js --js js/constants.js --js js/pif.js --js js/memory.js --js js/dma.js --js js/interrupts.js --js js/lib/glMatrix-0.9.5.min.js --js js/lib/webgl-utils.js --js js/gfxHelpers.js --js js/videoHLE.js --js js/webGL.js --js js/tests.js --js js/lib/bitjs/io.js --js js/lib/bitjs/archive.js --js js/ui.js --js_output_file bin/js/1964js-0.0.3.min.js
 
 #move files into place
 cp 1964js.html bin/
