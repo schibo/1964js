e8e993b322803db1a2766caaa3dc1334c59f99b0
schibo4@gmail.com <schibo4@gmail.com@0378edba-076e-5dc0-2bb2-d87a714dcd81>
2012-09-27 01:14:56 +0000

    latest version of closure compiler
    git-svn-id: http://1964js.googlecode.com/svn/trunk@136 0378edba-076e-5dc0-2bb2-d87a714dcd81

--- compile.sh
+++ compile.sh
@@ -26,5 +26,5 @@ java -jar compiler.jar --js lib/bitjs/unzip-modded-to-point-to-minified.js --js_
 echo done.
 
 echo minifying as $minified ...
-java -jar compiler.jar --js lib/BigInt.js --js lib/closure/goog/base.js  --js obj/constants.js --js obj/1964.js --js obj/helpers.js --js obj/opcodeMap.js --js obj/boot.js --js lib/closure/goog/math/long.js --js obj/pif.js --js obj/memory.js --js obj/dma.js --js obj/interrupts.js --js lib/glMatrix-0.9.5.min.js --js lib/webgl-utils.js --js obj/renderer.js --js obj/videoHLE.js --js obj/gfxHelpers.js --js obj/webGL.js --js lib/bitjs/io.min.js --js lib/bitjs/archive.min.js --js obj/ui.js --js_output_file lib/$minified
+java -jar compiler.jar --version --js lib/BigInt.js --js lib/closure/goog/base.js  --js obj/constants.js --js obj/1964.js --js obj/helpers.js --js obj/opcodeMap.js --js obj/boot.js --js lib/closure/goog/math/long.js --js obj/pif.js --js obj/memory.js --js obj/dma.js --js obj/interrupts.js --js lib/glMatrix-0.9.5.min.js --js lib/webgl-utils.js --js obj/renderer.js --js obj/videoHLE.js --js obj/gfxHelpers.js --js obj/webGL.js --js lib/bitjs/io.min.js --js lib/bitjs/archive.min.js --js obj/ui.js --js_output_file lib/$minified
 echo done.
