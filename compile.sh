d57da3fc387aabc7e518d719518bce968db7805d
schibo4@gmail.com <schibo4@gmail.com@0378edba-076e-5dc0-2bb2-d87a714dcd81>
2012-12-09 07:18:09 +0000

    cleanup slim code and improve script
    git-svn-id: http://1964js.googlecode.com/svn/trunk@185 0378edba-076e-5dc0-2bb2-d87a714dcd81

--- compile.sh
+++ compile.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
 minified="1964js-0.0.17.min.js"
+
+echo Compiling index.html
 slimrb index.slim --pretty index.html
+if test $? -ne 0
+then
+    echo Compilation aborted. Please fix slim errors first.
+    exit $?
+else
+    echo done.
+fi
+
+echo Compiling debug.html
 slimrb index.slim --pretty debug.html
+if test $? -ne 0
+then
+    echo Compilation aborted. Please fix slim errors first.
+    exit $?
+else
+    echo done.
+fi
 
 mkdir obj/
 mkdir css/
@@ -14,29 +32,32 @@ coffeelint -f coffeelint.json coffee/*.coffee
 if test $? -ne 0
 then
     echo Compilation aborted. Please fix CoffeeLint errors first.
+    exit $?
 else
     echo done.
-    echo Compiling coffee files ...
-    coffee -c -o obj/ coffee/ 
-    if test $? -ne 0
-    then
-        echo Minification aborted. Please fix CoffeeScript errors first.
-    else
-        echo done.
-        echo minifying io.js as io.min.js ...
-        java -jar compiler.jar --js lib/bitjs/io.js --js_output_file lib/bitjs/io.min.js
-        echo done.
+fi
 
-        echo minifying archive js as archive.min.js ...
-        java -jar compiler.jar --js lib/bitjs/archive-modded-to-point-to-minified.js --js_output_file lib/bitjs/archive.min.js
-        echo done.
+echo Compiling coffee files ...
+coffee -c -o obj/ coffee/ 
+if test $? -ne 0
+then
+    echo Minification aborted. Please fix CoffeeScript errors first.
+    exit $?
+else
+    echo done.
+    echo minifying io.js as io.min.js ...
+    java -jar compiler.jar --js lib/bitjs/io.js --js_output_file lib/bitjs/io.min.js
+    echo done.
 
-        echo minifying unzip.js as unzip.min.js ...
-        java -jar compiler.jar --js lib/bitjs/unzip-modded-to-point-to-minified.js --js_output_file lib/bitjs/unzip.min.js
-        echo done.
+    echo minifying archive js as archive.min.js ...
+    java -jar compiler.jar --js lib/bitjs/archive-modded-to-point-to-minified.js --js_output_file lib/bitjs/archive.min.js
+    echo done.
 
-        echo minifying as $minified ...
-        java -jar compiler.jar --version --js obj/constants.js --js lib/BigInt.js --js lib/closure/goog/base.js --js obj/1964.js --js obj/helpers.js --js obj/opcodeMap.js --js obj/boot.js --js lib/closure/goog/math/long.js --js obj/pif.js --js obj/keyboard.js --js obj/memory.js --js obj/gen.js --js obj/audio.js --js obj/dma.js --js obj/interrupts.js --js lib/glMatrix-0.9.5.min.js --js lib/webgl-utils.js --js obj/renderer.js --js obj/videoHLE.js --js obj/gfxHelpers.js --js obj/webGL.js --js lib/bitjs/io.min.js --js lib/bitjs/archive.min.js --js obj/ui.js --js_output_file lib/$minified
-        echo done.
-    fi
+    echo minifying unzip.js as unzip.min.js ...
+    java -jar compiler.jar --js lib/bitjs/unzip-modded-to-point-to-minified.js --js_output_file lib/bitjs/unzip.min.js
+    echo done.
+
+    echo minifying as $minified ...
+    java -jar compiler.jar --version --js obj/constants.js --js lib/BigInt.js --js lib/closure/goog/base.js --js obj/1964.js --js obj/helpers.js --js obj/opcodeMap.js --js obj/boot.js --js lib/closure/goog/math/long.js --js obj/pif.js --js obj/keyboard.js --js obj/memory.js --js obj/gen.js --js obj/audio.js --js obj/dma.js --js obj/interrupts.js --js lib/glMatrix-0.9.5.min.js --js lib/webgl-utils.js --js obj/renderer.js --js obj/videoHLE.js --js obj/gfxHelpers.js --js obj/webGL.js --js lib/bitjs/io.min.js --js lib/bitjs/archive.min.js --js obj/ui.js --js_output_file lib/$minified
+    echo done.
 fi
